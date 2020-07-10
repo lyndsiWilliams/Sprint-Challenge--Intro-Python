f4fd72a7a5d63552ffb8809b4d8d3b492de3d582
lyndsiWilliams <kcatgirl@gmail.com>
2020-07-10 10:16:18 -0500

    oop1 complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,37 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# Base class: Vehicle
+
+class Vehicle():
+    pass
+
+
+# Branch route 1: Ground
+
+class GroundVehicle(Vehicle):
+    pass
+
+# Ground branches to the two types of ground vehicles:
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+
+# Branch route 2: Flight
+
+class FlightVehicle(Vehicle):
+    pass
+
+# Flight branches to the two types of flight vehicles:
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
